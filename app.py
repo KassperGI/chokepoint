75ce73a31658788b64828ef589935eeed54f265f
KassperGI <kassvakkom@gmail.com>
2026-06-11 10:27:05 +0530

    model changed to gemini 1.5 flash from gemini 2.5 flash

--- app.py
+++ app.py
@@ -225,7 +225,7 @@ if st.button("RUN DIAGNOSTIC PROTOCOL"):
         
         try:
             response = client.models.generate_content(
-                model='gemini-2.5-flash',
+                model='gemini-1.5-flash',
                 contents=prompt,
             )
             
